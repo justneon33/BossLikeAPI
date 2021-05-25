6a6624040256049ea5092593b1a30e9766bb9977
Neon <32961194+y9neon@users.noreply.github.com>
2021-05-25 15:57:22 +0300

    update: `Library.VERSION` updated to `0.0.2`

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -2,5 +2,5 @@ object Library {
     const val PACKAGE = "fun.kotlingang.bosslike.api"
     const val NAME = "BossLike"
     const val DESCRIPTION = "Multiplatform API Library For Bosslike.ru"
-    const val VERSION = "0.0.1"
+    const val VERSION = "0.0.2"
 }
